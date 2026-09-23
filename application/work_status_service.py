2698a3b3f99080423362223e3d94b1b4f9cf7d92
rusya-malina <rusiko.xum@gmail.com>
2026-09-23 14:47:17 +0500

    Use configured work status storage

--- application/work_status_service.py
+++ application/work_status_service.py
@@ -6,13 +6,13 @@ from datetime import datetime
 from typing import Any
 from zoneinfo import ZoneInfo
 
-from config import BOT_TIMEZONE, GROUPS_FILE, USERS_FILE
+from config import BOT_TIMEZONE, GROUPS_FILE, USERS_FILE, WORK_STATUS_FILE
 from data_models import user_name
-from organization import TEAM_GROUPS, build_employee_registry, normalize_employee_name
+from organization import TEAM_GROUPS, build_employee_registry
 from storage import load_json, update_json
 
 
-STATUS_FILE = "work_status.json"
+STATUS_FILE = WORK_STATUS_FILE
 
 
 def _today() -> str:
